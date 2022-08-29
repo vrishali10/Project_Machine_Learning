e2041d1d490856b707c26ae24bf40226160ec478
vrishali ghule <vrishali1357@gmail.com>
2022-08-29 12:02:51 +0530

    updated

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from housing.logger import logging
 
 app=Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    logging.info("We are testing logging mofdule.")
     return "CI/CD pipeline has been established"
 
 
